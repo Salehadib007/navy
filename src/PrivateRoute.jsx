4ce108a36699cf456fe76c6328d859f91b57d93a
Saleh Adib Hasnat <salehadib00@gmail.com>
2026-02-11 13:26:44 +0600

    sd

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { getAuth } from "../utils/auth";
+// import { getAuth } from "../utils/auth";
+import { useAuth } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const auth = getAuth();
+  const { auth } = useAuth();
   return auth ? children : <Navigate to="/login" />;
 };
 
